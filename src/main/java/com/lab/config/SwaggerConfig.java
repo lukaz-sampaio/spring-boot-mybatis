28c9d0e9fbe5d04081c2e5297c6ce5888da80390
Lucas Sampaio <lukaz.sampaio@gmail.com>
2020-02-15 18:41:25 -0300

    Documentando API com Swagger

---
+++ src/main/java/com/lab/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.lab.config;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ *
+ * @author lucas
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        Predicate<RequestHandler> basePackages = RequestHandlerSelectors.basePackage("com.lab");
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackages)
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
